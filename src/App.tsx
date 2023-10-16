0223f007dee8e4624ba36f4d29aab23bdbae4ef3
Lucas Fernandes <lucasfpd.santos@gmail.com>
2023-10-16 17:52:12 -0300

    add pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import logo from "./logo.svg";
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  const tal = 'teste'
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
